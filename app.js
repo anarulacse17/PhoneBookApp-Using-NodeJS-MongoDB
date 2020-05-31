03d2840d5e37cf05bc5dd0c8b5f3327e595861d7
anarulacse17 <53555131+anarulacse17@users.noreply.github.com>
2020-05-31 22:10:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+
 var util = require('util'),
     express = require('express'),
-    expressValidator = require('express-validator');
-var swig = require('swig');
+    
+   swig = require('swig');
 var methodOverride = require('method-override');
 require("./contact");
 
@@ -28,11 +30,11 @@ app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(expressValidator());
 
 app.use('/contacts',contacts);
 
